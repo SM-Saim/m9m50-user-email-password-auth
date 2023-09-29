efe4dbe6835b10c0f145fe6d815bb9d9cb4a1e35
SM-Saim <saim777m@example.com>
2023-09-30 01:24:17 +0600

    regular expression validation and toggle show password

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,12 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import auth from "../../firebase/firebase";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const Register = () => {
   const [registerError, setRegisterError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
@@ -18,6 +20,9 @@ const Register = () => {
 
     if (password.length < 6) {
       setRegisterError("Password should be at least 6 or more character");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError("You should give atleast one upper case in password");
       return;
     }
 
@@ -50,13 +55,23 @@ const Register = () => {
         />
         <br />
         <input
-          className="mb-5 w-2/5 text-xl py-2 pl-1 rounded-md"
-          type="password"
+          className="mb-5 w-2/5 text-xl py-2 pl-1 rounded-md  relative"
+          type={showPassword ? "text" : "password"}
           name="password"
           id=""
           placeholder="Enter Password"
           required
         />
+        <span
+          className="absolute ml-[-30px] mt-[13px] text-xl "
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? (
+            <AiFillEyeInvisible></AiFillEyeInvisible>
+          ) : (
+            <AiFillEye></AiFillEye>
+          )}
+        </span>
         <br />
         <input
           className="btn btn-secondary w-2/5"
